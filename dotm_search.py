3a79ab73e892f6dbb1b6056b8f05c7c8737e0c6b
Carl Smith <carlfsmithiii@gmail.com>
2018-11-19 03:21:28 -1000

    work towards reading dotm files

--- dotm_search.py
+++ dotm_search.py
@@ -7,19 +7,53 @@ within the 'word/document.xml' section of a MSWord .dotm file.
 # Your awesome code begins here!
 import argparse
 import os
+import sys
+from oletools.olevba3 import (
+    VBA_Parser,
+    TYPE_OLE,
+    TYPE_OpenXML,
+    TYPE_Word2003_XML,
+    TYPE_MHTML,
+)
 
 
 def check_args(args=None):
     parser = argparse.ArgumentParser(
-        description='Search DOTM files in a directory for \'$\' character')
-    parser.add_argument('search_string', default=os.getcwd(), help='string to search for in DOTM files')
-    parser.add_argument('--dir', metavar='directory name',
-                        type=str, help='name of directory containing DOTM files')
+        description="Search DOTM files in a directory for '$' character"
+    )
+    parser.add_argument("search_string", help="string to search for in DOTM files")
+    parser.add_argument(
+        "--dir",
+        default=os.getcwd(),
+        metavar="directory name",
+        type=str,
+        help="name of directory containing DOTM files",
+    )
     arguments = parser.parse_args(args)
     return (arguments.search_string, arguments.dir)
 
-def search_directory(search_string, dir_name = '.'):
-    
 
-if __name__ == '__main__':
+def search_directory(dir_name, search_string):
+    filenames = [
+        filename
+        for filename in os.listdir(dir_name)
+        if filename.lower().endswith(".dotm")
+    ]
+    for filename in filenames:
+        # with open(filename, "rb") as f_obj:
+        #     f_data = f_obj.read()
+        #     vbaparser = VBA_Parser(filename, f_data)
+        #     # try:
+        #     #     print(vbaparser.reveal())
+        #     # except Exception:
+        #     #     pass
+        #     # finally:
+        #     #     vbaparser.close()
+        document = Document(filename)
+        print(dir(document))
+        document.close()
+
+
+if __name__ == "__main__":
     search_string, directory = check_args(sys.argv[1:])
+    search_directory(directory, search_string)
